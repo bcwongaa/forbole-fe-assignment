aa2c6f5c7db4aa7727774e99b14cb754ae3526bb
bcwongaa <bcwongaa@gmail.com>
2023-11-27 21:33:41 +0800

    add test case

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,57 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders the board', () => {
+  const { getAllByRole } = render(<App />);
+  const squares = getAllByRole('button');
+
+  // 9 box with 1 restart button
+  expect(squares.length).toBe(10);
+});
+
+test('User clicks the boards and wins', () => {
+  const { getAllByRole } = render(<App />);
+  const squares = getAllByRole('button');
+
+  // 9 box with 1 restart button
+  fireEvent.click(squares[0]);
+  fireEvent.click(squares[3]);
+  fireEvent.click(squares[1]);
+  fireEvent.click(squares[4]);
+  fireEvent.click(squares[2]);
+  const status = screen.getByText('X wins!');
+  expect(status).toBeInTheDocument();
+});
+
+test('User clicks the reset should clear the board', () => {
+  const { getAllByRole } = render(<App />);
+  const squares = getAllByRole('button');
+
+  // 9 box with 1 restart button
+  fireEvent.click(squares[0]);
+  fireEvent.click(squares[3]);
+  fireEvent.click(squares[1]);
+  fireEvent.click(squares[4]);
+  fireEvent.click(squares[2]);
+
+  fireEvent.click(squares[9]); // restart game button
+  const status = screen.getByText('Player X turn:');
+  expect(status).toBeInTheDocument;
+});
+
+test('Reload should keeps the boards status', () => {
+  const { getAllByRole } = render(<App />);
+  const squares = getAllByRole('button');
+
+  // 9 box with 1 restart button
+  fireEvent.click(squares[0]);
+  fireEvent.click(squares[3]);
+  fireEvent.click(squares[1]);
+  fireEvent.click(squares[4]);
+  fireEvent.click(squares[2]);
+
+  window.location.reload();
+
+  const status = screen.getByText('X wins!');
+  expect(status).toBeInTheDocument();
 });
